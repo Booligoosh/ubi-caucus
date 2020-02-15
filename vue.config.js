d917052b3f6da7eeeb47fa00498e30cf20975559
Ethan <me@ethan.link>
2020-02-15 23:33:55 +1100

    Make extra sure to bust the cache just in case

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ module.exports = {
     name: `UBI Caucus`,
     workboxOptions: {
       exclude: [`_redirects`, `_headers`],
+      // https://forum.vuejs.org/t/vue-cli-3-project-with-pwa-not-busting-cache-on-new-builds/50252/7
       skipWaiting: true,
       clientsClaim: true
     }

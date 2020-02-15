2915fe559a8f6816c8535321bfd8dadf538fc27a
Ethan <me@ethan.link>
2020-02-15 15:34:40 +1100

    Added a search bar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="home">
     <h1>Candidates supporting Universal Basic Income</h1>
+    <input
+      type="text"
+      class="search"
+      v-model="search"
+      placeholder="Search by name or district code"
+    />
     <div class="people">
       <PersonCard
-        v-for="person of $store.state.people"
+        v-for="person of peopleToShow"
         :key="person.id"
         :person="person"
       />
@@ -18,6 +24,25 @@ export default {
   name: "Home",
   components: {
     PersonCard
+  },
+  data() {
+    return {
+      search: ``
+    };
+  },
+  computed: {
+    peopleToShow() {
+      const people = this.$store.state.people;
+      if (this.search) {
+        return people.filter(person =>
+          `${person.name} ${person.area}`
+            .toLowerCase()
+            .includes(this.search.toLowerCase())
+        );
+      } else {
+        return people;
+      }
+    }
   }
 };
 </script>
@@ -39,5 +64,25 @@ export default {
   .people {
     grid-template-columns: 1fr;
   }
+}
+
+.search {
+  margin-bottom: 2rem;
+  padding: 1rem;
+  border: none;
+  font-size: 1em;
+  border-radius: 5px;
+  width: 100%;
+  background: hsl(210, 29%, 90%);
+}
+
+.search::placeholder {
+  color: inherit;
+  opacity: 0.5;
+}
+
+.search:focus {
+  outline: none;
+  box-shadow: inset 0 0 0 1px hsl(210, 29%, 85%);
 }
 </style>

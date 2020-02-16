cc0e7353a2e1a0fbe87b18e61f07c48a73282542
Ethan <me@ethan.link>
2020-02-16 20:47:46 +1100

    Added a JS redirect too

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
+if (window.location.hostname === "ubi-caucus.netlify.com") {
+  window.location.hostname = "support.us.openubiproject.org";
+}
+
 new Vue({
   router,
   render: h => h(App)

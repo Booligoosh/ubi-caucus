20387d0a68cadb74a9d2b0d13ceab47f78e91c2e
Ethan <me@ethan.link>
2020-02-18 15:26:39 +1100

    Updated about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,11 +2,33 @@
   <div class="about">
     <h1>About this site</h1>
     <p>
-      This site was built by
+      This site was programmed by
       <a href="https://twitter.com/KoalasForYang" target="_blank" rel="noopener"
         >@KoalasForYang</a
+      >, as part of the Open UBI Project.
+    </p>
+    <p>
+      It was built in collaboration with
+      <a
+        href="https://twitter.com/StevenPenderg11"
+        target="_blank"
+        rel="noopener"
+        >@StevenPenderg11</a
       >
-      as part of the Open UBI Project. Data is from the Open UBI Project.
+      and
+      <a href="https://twitter.com/kp_colussi" target="_blank" rel="noopener"
+        >@kp_colussi</a
+      >.
+    </p>
+    <p>
+      The data is maintained by the Open UBI Project. Please contact
+      <a
+        href="https://twitter.com/StevenPenderg11"
+        target="_blank"
+        rel="noopener"
+        >@StevenPenderg11</a
+      >
+      if you have any questions!
     </p>
   </div>
 </template>

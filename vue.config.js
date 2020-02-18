3d766f3df45d7e05cf24ad1fde5733cca1d38947
Ethan <me@ethan.link>
2020-02-18 20:13:17 +1100

    Actually fixed PWA env stuff lol

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,16 @@
-require("dotenv").config();
-
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
 
 module.exports = {
   pwa: {
     name:
-      process.env.url === `https://support.us.openubiproject.org`
+      process.env.URL === `https://support.us.openubiproject.org`
         ? `Candidates supporting Universal Basic Income`
         : `UBI Caucus`,
     themeColor: `#2b3d4f`,
     manifestOptions: {
       short_name:
-        process.env.url === `https://support.us.openubiproject.org`
+        process.env.URL === `https://support.us.openubiproject.org`
           ? `UBI Candidates`
           : `UBI Caucus`,
       background_color: `#243665`

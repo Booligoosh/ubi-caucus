6553f2ae05acf33cc6a8d163069eef39065f7041
Ethan <me@ethan.link>
2020-02-22 17:10:01 +1100

    Updated about page

--- src/views/splash/About.vue
+++ src/views/splash/About.vue
@@ -7,6 +7,16 @@
       United States of America, building on top of the
       <a href="https://openubiproject.org">OpenUBI</a> best practices.
     </p>
+    <h2>Our logo</h2>
+    <p>
+      The &ldquo;≥&rdquo; sign symbolizes the floor that UBI builds under the
+      feet of everyone. UBI says that income should be ≥ the value that people
+      put into the economic machine because we all have inherent human value.
+      Our logo was made by the wonderful
+      <a href="https://twitter.com/JosephKChoi" target="_blank" rel="noopener"
+        >Joseph Choi</a
+      >.
+    </p>
     <h2>Contact</h2>
     <p>
       The UBI Caucus is run by
@@ -22,7 +32,11 @@
     </p>
     <h2>Credits</h2>
     <p>
-      This website is part of the OpenUBI Action Framework and is built by
+      This website is part of the OpenUBI Action Framework founded by
+      <a href="https://twitter.com/kp_colussi" target="_blank" rel="noopener"
+        >Kevin Colussi</a
+      >
+      and is built by
       <a href="https://twitter.com/KoalasForYang" target="_blank" rel="noopener"
         >@KoalasForYang</a
       >.
